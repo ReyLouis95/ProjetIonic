5c50aebbaf3e4bc4f45f9b2ffc2fffde3b2476dc
MOSvisio <lucas.schutz@vivoka.com>
2020-05-20 18:15:52 +0200

    un système de tri dans la Tab 1 avec la searchbar

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
 
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
 
-  constructor() { }
+  offres: Observable<any>;
+
+  constructor(public httpClient: HttpClient) { }
+
+  getAllOffres() {
+    return this.httpClient.get("https://webservice-rest-velo-back.herokuapp.com/product");
+  }
+
 }
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ApiService } from '../api.service'
 
 @Component({
   selector: 'app-tab1',
@@ -6,7 +7,25 @@ import { Component } from '@angular/core';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+  offres: any;
 
-  constructor() {}
+  constructor(public api : ApiService) {
+    api.getAllOffres().subscribe(data => {
+      this.offres = data['products'];
+      console.log(this.offres)
+    });
+    
+  }
+
+  onSearchChange(event){
+    let items = Array.from(document.querySelector('ion-list').children) as HTMLElement[];
+    console.log(event.target.value)
+    requestAnimationFrame(() => {
+      items.forEach(item => {
+        const shouldShow = item.textContent.toLowerCase().includes(event.target.value.toLowerCase());
+        item.style.display = shouldShow ? 'block' : 'none';
+      });
+    });
+  }
 
 }
