14dac6018da15a05097cc28371dba262bdeeefce
Louis REY <louis.rey95@gmail.com>
2020-05-21 19:02:49 +0200

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
+import { FormsModule} from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
@@ -14,7 +16,7 @@ import { AppComponent } from './app.component';
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule, FormsModule, ReactiveFormsModule,],
   providers: [
     StatusBar,
     SplashScreen,
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,12 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {
 
-  constructor() {}
+export class Tab2Page implements OnInit {
+  loginForm: FormGroup;
 
+  constructor(private fb: FormBuilder) { }
+
+  ngOnInit() {
+    this.loginForm = this.fb.group({  // Crée une instance de FormGroup
+      nom: [''],                   // Crée une instance de FormControl
+      prenom: [''],
+      ville: [''],
+      email: [''],
+      telephone: [''],
+      contrat: [''],
+      emploi: [''],
+      localisation: [''], 
+      rayon:[''],
+      disponibilite: [''],
+    });
+  }
+
+  login() {
+    console.log('Données du formulaire...', this.loginForm.value);
+  }
 }
